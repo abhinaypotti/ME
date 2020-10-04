dfbe212046dbf68d32ba58092e27b87682d5269b
abhinaypotti <pottiabhinay2001@gmail.com>
2020-10-04 21:12:44 +0530

    Updates

---
+++ precache-manifest.d717d43ab104efc9c18e6ddc994fbf6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e13efb99f633b040099a3fe870dd65",
+    "url": "/ME/index.html"
+  },
+  {
+    "revision": "1b41106691d1e796d323",
+    "url": "/ME/static/css/main.43b24836.chunk.css"
+  },
+  {
+    "revision": "0be956e65a21e6406720",
+    "url": "/ME/static/js/2.065e226e.chunk.js"
+  },
+  {
+    "revision": "0261e2dee4a7e8969069abb914658242",
+    "url": "/ME/static/js/2.065e226e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b41106691d1e796d323",
+    "url": "/ME/static/js/main.fdd664a9.chunk.js"
+  },
+  {
+    "revision": "6df5681a44ebd62352c4",
+    "url": "/ME/static/js/runtime-main.8fc480aa.js"
+  }
+]);
